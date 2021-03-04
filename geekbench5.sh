78cc3f25767104da805c71f5e0893114f0001bcb
Nuno Miguel Nobre <nunomiguel.nobre@manchester.ac.uk>
2021-03-04 01:01:01 +0000

    Check the existence of and execute w/ wget/curl

--- geekbench5.sh
+++ geekbench5.sh
@@ -1,8 +1,21 @@
+if ! command -v wget &> /dev/null && ! command -v curl &> /dev/null
+then
+    echo "Neither wget nor curl were found. Please install one of them."
+    exit
+fi
+
 url=$(./geekbench5 | grep "claim?key" -m 1 | sed -e 's/^[ \t]*//')
 url_trim=$(echo $url | sed -e 's/claim.*//')
 
-SC=$(wget --no-check-certificate -O - $url_trim 2>&1 | grep -e "Single-Core Score" -B 1 -m 1 | sed -r 's/.*>([0-9]*)<.*/\1/' | head -n 1)
-MC=$(wget --no-check-certificate -O - $url_trim 2>&1 | grep -e "Multi-Core Score" -B 1 -m 1 | sed -r 's/.*>([0-9]*)<.*/\1/' | head -n 1)
+if command -v wget &> /dev/null
+then
+    SC=$(wget --no-check-certificate -O - $url_trim 2>&1 | grep -e "Single-Core Score" -B 1 -m 1 | sed -r 's/.*>([0-9]*)<.*/\1/' | head -n 1)
+    MC=$(wget --no-check-certificate -O - $url_trim 2>&1 | grep -e "Multi-Core Score" -B 1 -m 1 | sed -r 's/.*>([0-9]*)<.*/\1/' | head -n 1)
+elif command -v curl &> /dev/null
+then
+    SC=$(curl -k $url_trim 2>&1 | grep -e "Single-Core Score" -B 1 -m 1 | sed -r 's/.*>([0-9]*)<.*/\1/' | head -n 1)
+    MC=$(curl -k $url_trim 2>&1 | grep -e "Multi-Core Score" -B 1 -m 1 | sed -r 's/.*>([0-9]*)<.*/\1/' | head -n 1)
+fi
 
 echo $url
 echo "Single-Core Score:" $SC
